d01c5884906b6048be9194724247a482a62ad80b
Antonio Ruberto <anto.ruberto@gmail.com>
2015-02-08 20:27:06 -0500

    java.time initial version

--- business-time-jdk8/src/main/java/com/github/aruberto/businesstime/jdk8/BusinessDateTime.java
+++ business-time-jdk8/src/main/java/com/github/aruberto/businesstime/jdk8/BusinessDateTime.java
@@ -7,6 +7,7 @@ import com.github.aruberto.businesstime.common.Constants;
 import net.objectlab.kit.datecalc.common.DateCalculator;
 import net.objectlab.kit.datecalc.common.DefaultHolidayCalendar;
 import net.objectlab.kit.datecalc.common.HolidayHandlerType;
+import net.objectlab.kit.datecalc.common.WorkingWeek;
 import net.objectlab.kit.datecalc.jdk8.Jdk8WorkingWeek;
 import net.objectlab.kit.datecalc.jdk8.LocalDateKitCalculatorsFactory;
 
@@ -18,11 +19,15 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.chrono.ChronoLocalDateTime;
 import java.time.chrono.ChronoZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalField;
 import java.time.temporal.TemporalUnit;
 import java.util.Collections;
 import java.util.Set;
+
+import static java.time.LocalTime.MICROS_PER_DAY;
+import static java.time.LocalTime.MILLIS_PER_DAY;
 
 public final class BusinessDateTime
     implements Temporal, ChronoZonedDateTime<LocalDate>, Serializable {
@@ -53,7 +58,7 @@ public final class BusinessDateTime
                           LocalTime dayStartTime,
                           LocalTime dayEndTime,
                           Set<LocalDate> holidays,
-                          Jdk8WorkingWeek workingWeek) {
+                          WorkingWeek workingWeek) {
     if (dateTime == null) {
       throw new NullPointerException("date time cannot be null");
     }
@@ -67,7 +72,7 @@ public final class BusinessDateTime
       holidays = Collections.emptySet();
     }
     if (workingWeek == null) {
-      workingWeek = Jdk8WorkingWeek.DEFAULT;
+      workingWeek = WorkingWeek.DEFAULT;
     }
     if (dayEndTime.equals(dayStartTime) || dayEndTime.isBefore(dayStartTime)) {
       throw new IllegalArgumentException("business day end time must be after start time");
@@ -76,7 +81,105 @@ public final class BusinessDateTime
     this.dayStartTime = dayStartTime;
     this.dayEndTime = dayEndTime;
     this.holidays = holidays;
-    this.workingWeek = workingWeek;
+    this.workingWeek = new Jdk8WorkingWeek(workingWeek);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at point of time of {@code dateTime}
+   * with business day starting at {@code dayStartTime}, business day ending at {@code dayEndTime},
+   * holiday list of {@code holidays} and working week of Monday to Friday
+   *
+   * @param dateTime date time, null means current time with default time zone
+   * @param dayStartTime business day start time, null means 9am
+   * @param dayEndTime business day end time, null means 5pm
+   * @param holidays holidays, null means no holidays
+   */
+  public BusinessDateTime(ZonedDateTime dateTime,
+                          LocalTime dayStartTime,
+                          LocalTime dayEndTime,
+                          Set<LocalDate> holidays) {
+    this(dateTime, dayStartTime, dayEndTime, holidays, null);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at current time in system time zone
+   * with business day starting at {@code dayStartTime}, business day ending at {@code dayEndTime},
+   * holiday list of {@code holidays} and working week of Monday to Friday
+   *
+   * @param dayStartTime business day start time, null means 9am
+   * @param dayEndTime business day end time, null means 5pm
+   * @param holidays holidays, null means no holidays
+   */
+  public BusinessDateTime(LocalTime dayStartTime, LocalTime dayEndTime, Set<LocalDate> holidays) {
+    this(ZonedDateTime.now(), dayStartTime, dayEndTime, holidays, null);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at point of time of {@code dateTime}
+   * with business day starting at 9am, business day ending at 5pm,
+   * holiday list of {@code holidays} and working week of Monday to Friday
+   *
+   * @param dateTime date time, null means current time with default time zone
+   * @param holidays holidays, null means no holidays
+   */
+  public BusinessDateTime(ZonedDateTime dateTime, Set<LocalDate> holidays) {
+    this(dateTime, null, null, holidays, null);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at current time in system time zone
+   * with business day starting at 9am, business day ending at 5pm,
+   * holiday list of {@code holidays} and working week of Monday to Friday
+   *
+   * @param holidays holidays, null means no holidays
+   */
+  public BusinessDateTime(Set<LocalDate> holidays) {
+    this(ZonedDateTime.now(), null, null, holidays, null);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at point of time of {@code dateTime}
+   * with business day starting at {@code dayStartTime}, business day ending at {@code dayEndTime},
+   * no holidays and working week of Monday to Friday
+   *
+   * @param dateTime date time, null means current time with default time zone
+   * @param dayStartTime business day start time, null means 9am
+   * @param dayEndTime business day end time, null means 5pm
+   */
+  public BusinessDateTime(ZonedDateTime dateTime, LocalTime dayStartTime, LocalTime dayEndTime) {
+    this(dateTime, dayStartTime, dayEndTime, null, null);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at current time in system time zone
+   * with business day starting at {@code dayStartTime}, business day ending at {@code dayEndTime},
+   * no holidays and working week of Monday to Friday
+   *
+   * @param dayStartTime business day start time, null means 9am
+   * @param dayEndTime business day end time, null means 5pm
+   */
+  public BusinessDateTime(LocalTime dayStartTime, LocalTime dayEndTime) {
+    this(ZonedDateTime.now(), dayStartTime, dayEndTime, null, null);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at point of time of {@code dateTime}
+   * with business day starting at 9am, business day ending at 5pm,
+   * no holidays and working week of Monday to Friday
+   *
+   * @param dateTime date time, null means current time with default time zone
+   */
+  public BusinessDateTime(ZonedDateTime dateTime) {
+    this(dateTime, null, null, null, null);
+  }
+
+  /**
+   * Constructs an instance of BusinessDateTime at current time in system time zone,
+   * with business day starting at 9am, business day ending at 5pm,
+   * no holidays and working week of Monday to Friday
+   */
+  public BusinessDateTime() {
+    this(ZonedDateTime.now(), null, null, null, null);
   }
 
   private BusinessDateTime moveByMillis(long millisToMove) {
@@ -172,6 +275,21 @@ public final class BusinessDateTime
 
   @Override
   public ChronoZonedDateTime<LocalDate> plus(long amountToAdd, TemporalUnit unit) {
+    /*
+    if (unit instanceof ChronoUnit) {
+      ChronoUnit f = (ChronoUnit) unit;
+      switch (f) {
+        case NANOS: return plusNanos(amountToAdd);
+        case MICROS: return plusDays(amountToAdd / MICROS_PER_DAY).plusNanos((amountToAdd % MICROS_PER_DAY) * 1000);
+        case MILLIS: return plusDays(amountToAdd / MILLIS_PER_DAY).plusNanos((amountToAdd % MILLIS_PER_DAY) * 1000_000);
+        case SECONDS: return plusSeconds(amountToAdd);
+        case MINUTES: return plusMinutes(amountToAdd);
+        case HOURS: return plusHours(amountToAdd);
+        case HALF_DAYS: return plusDays(amountToAdd / 256).plusHours((amountToAdd % 256) * 12);  // no overflow (256 is multiple of 2)
+      }
+      return with(date.plus(amountToAdd, unit), time);
+    }
+    */
     return new BusinessDateTime(
         dateTime.plus(amountToAdd, unit),
         dayStartTime,
@@ -183,7 +301,7 @@ public final class BusinessDateTime
 
   @Override
   public long until(Temporal endExclusive, TemporalUnit unit) {
-    return dateTime.until(endExclusive, unit);
+    return moveByMillis(0).dateTime.until(endExclusive, unit);
   }
 
   @Override
